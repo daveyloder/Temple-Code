a70a88a28bf2cb53d2f6d3cd6e39fccef0f78fd7
daveyloder <davidloder92@gmail.com>
2022-10-18 14:58:22 -0400

    Changed code to find most common word in a txt doc

--- CIS 1051/Project Gutenberg/readBook.py
+++ CIS 1051/Project Gutenberg/readBook.py
@@ -1,7 +1,22 @@
 data = open("./data/dracula.txt", "r", encoding="utf-8")
-text = data.read()
-
-lines = text.split("\n")
-
+count = {}
+lines = data.readlines()
+# print(lines)
 for line in lines:
-    print(line)
+    words = line.split()
+    # print(words)
+    for word in words:
+        word = word.strip()
+        word = word.lower()
+        if word[-1] in ".',?!--_":
+            word = word[:-1]
+
+        if word not in count:
+            count[word] = 1
+            # print("new word!")
+            # print(count)
+            # print("----------")
+        else:
+            count[word] += 1
+for word in sorted(count, key=count.get, reverse=True)[:20]:
+    print(word, count[word])
