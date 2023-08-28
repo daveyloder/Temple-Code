95fc61c19fa0067c53ab7cf0d1f866bcb601d3d6
daveyloder <davidloder92@gmail.com>
2023-08-28 10:59:18 -0400

    Added Program Design and Abstraction course folder

---
+++ CIS 1068/assignment01.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class assignment01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("// David Loder");
+        System.out.println("// " + date);
+        System.out.println("// Assignment 01. Hello there!");
+        System.out.println(
+                "// Thank you for interest in running this basic program. It is designed to print my name, give you today's current date, and give you this description of how this program should work. IF you have any questions feel free to ask away!... Okay that's it, there's no more of the program it is done... You're still here?");
+
+    }
+}
